6349236fe2bb6ca3d4967850c100eaebf283b4e0
Yihan CHENG <chengyihan1993@gmail.com>
2021-02-15 09:34:29 +0100

    test global var

--- vars/useHello.groovy
+++ vars/useHello.groovy
@@ -2,3 +2,9 @@ def useHelloFun(String str) {
     println(str)
     hello(str)
 }
+
+def runShellCheck() {
+    def output = sh(script: "mkdir -p ./output/shellcheck",
+                    returnStdout: true).trim()
+    println (output)
+}
